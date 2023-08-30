06fea6c2cb11c8a4b8c7b93adf6182582c94ecfb
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2023-08-30 08:47:12 -0300

    Update Punto 5.c

--- TP3- Recursividad/Punto 5.c
+++ TP3- Recursividad/Punto 5.c
@@ -21,10 +21,10 @@ int main()
 void confirmarCapicua(int arreglo[], int validos, int i, int k) {
     if (i >= k) { // 1° condicion de salida, que los dos indices sean iguals o uno mayor a otro
         printf("Es capicua");
-        return;
-    }
 
-    if (arreglo[i] != arreglo[k]) { // 2° condicion de slaida, que los elelemntos sean diferentes
+    }
+     else if (arreglo[i] != arreglo[k]){
+      // 2° condicion de slaida, que los elelemntos sean diferentes
         printf("No es capicua");
     } else {
         confirmarCapicua(arreglo, validos, i + 1, k - 1); // llamado recursivo
@@ -39,14 +39,14 @@ void confirmarCapicua(int arreglo[], int validos, int i, int k) {
 /*
 void confirmarCapicua(int arreglo[], int validos, int i, int k){
 
-if ( i >= k && arreglo[i] == arreglo[k] ){ 
+if ( i >= k && arreglo[i] == arreglo[k] ){
 
 
-// Para que salga de la funcion, lo que tiene que 
+// Para que salga de la funcion, lo que tiene que
     pasar es que los indices sean iguales, lo cual si sucede quere decir que es capicua
     y tambien que los valores sean diferentes, por lo tanto si sucede no es capicua
-    
-    
+
+
         printf(" es capicua");
 } else{
         printf(" no Es capicua");
@@ -57,3 +57,8 @@ if ( i >= k && arreglo[i] == arreglo[k] ){
 }
 
 */
+
+
+
+
+
